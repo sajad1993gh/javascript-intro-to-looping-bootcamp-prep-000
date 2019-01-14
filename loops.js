11d6b27d02ff024ee4d9d0f5ff8fb1a6b21429c0
Sajad Gholamzadehrizi <sajad1993gh@gmail.com>
2019-01-14 18:09:03 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,7 +1,12 @@
 function forLoop(array){
   
   for (var i = 0; i < 25; i++) {
-  console.log("I am " + i + " strange loop");
+  if (i===1){
+    console.log("I am " + i + " strange loop.");
+  }
+  else {
+    console.log("I am ${i} strange loops.");
+  }
   }
 
 }
